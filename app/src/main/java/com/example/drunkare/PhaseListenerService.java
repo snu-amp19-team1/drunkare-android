bde223054db6040ca33540e28d35bd70125f6029
eric-yoo <ajoije729@gmail.com>
2019-03-24 23:34:03 +0900

    basic background service

---
+++ app/src/main/java/com/example/drunkare/PhaseListenerService.java
@@ -0,0 +1,45 @@
+package com.example.drunkare;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+import static android.content.ContentValues.TAG;
+
+public class PhaseListenerService extends Service {
+
+    public Context context = this;
+    public Handler handler = null;
+    public static Runnable runnable = null;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+    @Override
+    public void onCreate() {
+        handler = new Handler();
+        runnable = new Runnable() {
+            public void run() {
+                Toast.makeText(context, "Service is still running", Toast.LENGTH_LONG).show();
+                handler.postDelayed(runnable, 10000);
+            }
+        };
+        handler.postDelayed(runnable, 15000);
+    }
+
+    @Override
+    public void onDestroy() {
+//        Toast.makeText(this, "Service stopped", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onStart(Intent intent, int startid) {
+        Toast.makeText(this, "Service started by user.", Toast.LENGTH_LONG).show();
+    }
+
+}
